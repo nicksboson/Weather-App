9388fa7d5a3c55a13e04981bafd84516ec685a5f
Nikhil garkoti <nikhil.garkoti0001@gmail.com>
2025-07-05 02:12:04 +0530

    some changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // for root deployment, or '/your-repo-name/' if using GitHub Pages
 })
